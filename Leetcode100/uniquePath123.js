4e8509e1408fa681735a5b126283a06582942c90
yi <mavisluan83@gmail.com>
2019-10-29 14:46:33 -0700

    62-63-980 uniquePath1 uniquePath2 uniquePath3 Dynamic Programming Backtrack

--- Leetcode100/uniquePath123.js
+++ Leetcode100/uniquePath123.js
@@ -51,9 +51,9 @@ const uniquePaths1 = (m, n) => {
         }
     }
 
-    console.log('matrix', matrix)
+    console.log('matrix', matrix);
     return matrix[n - 1][m - 1];
-}
+};
 
 // console.log('uniquePaths', uniquePaths1(4, 3))
 
@@ -111,7 +111,7 @@ const uniquePath2 = (obstacleGrid) => {
         }
     }
     return matrix[r - 1][c - 1];
-}
+};
 
 
 // console.log('uniquePath2', uniquePath2([
@@ -124,4 +124,91 @@ const uniquePath2 = (obstacleGrid) => {
 // console.log('uniquePath2', uniquePath2([[1, 0]]))
 
 // console.log('uniquePath2', uniquePath2([[1],[0]]))
-console.log('uniquePath2', uniquePath2([[0,0,0,0,0],[0,0,0,0,1],[0,0,0,1,0],[0,0,0,0,0]]))
+// console.log('uniquePath2', uniquePath2([[0,0,0,0,0],[0,0,0,0,1],[0,0,0,1,0],[0,0,0,0,0]]))
+
+
+/*
+980. Unique Path3
+On a 2-dimensional grid, there are 4 types of squares:
+
+1 represents the starting square.  There is exactly one starting square.
+2 represents the ending square.  There is exactly one ending square.
+0 represents empty squares we can walk over.
+-1 represents obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square,
+that walk over every non-obstacle square exactly once.
+
+Example 1:
+
+Input: [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths:
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+Example 2:
+
+Input: [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths:
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+Example 3:
+
+Input: [[0,1],[2,0]]
+Output: 0
+Explanation:
+There is no path that walks over every empty square exactly once.
+Note that the starting and ending square can be anywhere in the grid.
+
+Note:
+1 <= grid.length * grid[0].length <= 20
+ */
+
+// Solution
+// Time: O(H * W * 2 ^H*W)     Space: O( H * W )
+function uniquePathsIII(grid) {
+    if (grid == null || grid.length === 0) return 0;
+
+    const h = grid.length;
+    const w = grid[0].length;
+    const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+    let start;
+    let end;
+    let emptyCount = 1;
+    let res = 0;
+
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            if (grid[i][j] === 0) emptyCount++;
+            else if (grid[i][j] === 1) start = [i, j];
+            else if (grid[i][j] === 2) end = [i, j];
+        }
+    }
+
+    function go(x, y, count) {
+        if (grid[x][y] === -1 || grid[x][y] === Infinity) return;   // if current position is a wall or visited, return
+
+        if (x === end[0] && y === end[1]) {
+            if (count === emptyCount) res++;
+            return;
+        }
+
+        grid[x][y] = Infinity; // Mark visited  (if not a wall or visited)
+        // check on 4 directions, if valid, keep moving, if all directions not valid, reset the position
+        for (const [di, dj] of dirs) {
+            const i = x + di;
+            const j = y + dj;
+            if (i < 0 || i >= h || j < 0 || j >= w) continue;  // if out of bound, continue
+            go(i, j, count + 1);  // increase count on every move
+        }
+        grid[x][y] = 0; // Reset current position
+    }
+
+    go(start[0], start[1], 0);
+    return res;
+}
+
+
+console.log('uniquePathsIII', uniquePathsIII([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, -1]]));
