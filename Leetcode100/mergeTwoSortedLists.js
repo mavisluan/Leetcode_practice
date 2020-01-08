b0929c066a06dc6bd90b1ee2b745daebead069ab
yi <mavisluan83@gmail.com>
2020-01-08 15:52:38 -0800

    Updated Iterative solution

--- Leetcode100/mergeTwoSortedLists.js
+++ Leetcode100/mergeTwoSortedLists.js
@@ -28,36 +28,45 @@ const [n4, n5] = [new ListNode(3), new ListNode(4)];
 head2.next = n4;
 n4.next = n5;
 
-// Time: O( m * n )
-const mergeTwoLists1 = (l1, l2) => {
-    let node = l1;
-    if (!l1) return l2;
-    if (!l2) return l1;
-    if (!l1 || !l2) return null;
-    // move node to the last node of l1
-    while (node.next) {
-        node = node.next;
-    }
-    // connect l1 with l2
-    node.next = l2;
-    node = l1;
+// Iterative
+// Time: O(m + n)  Space: O(1)
+/*
+    1. Create a preHead Node
+    2. Set a runner pointer pointing to the preHead Node
+    3. While l1 and l2 exists (l1, l2 are pointers that point to list 1's and list 2's nodes)
+        - compare l1.val with l2.val
+        - set runner.next = the smaller Node
+        - move the smallerNode's pointer forward
+        - move runner forward
+    4. Connect the last node
+        - if (l1 === null) runner.next = l2
+        - else runner.next = l1
+    5. Return the real head
+        - return preHead.next
+ */
+const mergeTwoLists1= (l1, l2) => {
+    const preHead = new ListNode(-1);
+    let runner = preHead;
 
-    while (node) {
-        let curr = node.next;
-
-        while (curr) {
-            if (node.val > curr.val) {
-                [curr.val, node.val] = [node.val, curr.val];
-            }
-            curr = curr.next;
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            runner.next = l1;
+            l1 = l1.next;
+        } else {
+            runner.next = l2;
+            l2 = l2.next;
         }
-        node = node.next;
+
+        runner = runner.next;
     }
-    return l1;
+
+    // only one of l1 and l2 exists
+    runner.next = l1 ? l1 : l2;
+
+    return preHead.next;
 };
 
 console.log('mergeTwoSortedLists1', mergeTwoLists1(head1, head2));
-
 
 // Recursive
 // 1. compare two nodes' vals
@@ -65,7 +74,7 @@ console.log('mergeTwoSortedLists1', mergeTwoLists1(head1, head2));
 // 3. Return the smaller node
 // Time: O(m+n) --> Each recursive call increments the pointer to l1 or l2 by one until one reach the end (null)
 // Space: O(m+n) --> The first call doesn't return until the ends of both l1 and l2 have reached (m +n) stacks
-var mergeTwoLists2 = function (l1, l2) {
+const mergeTwoLists2 = function (l1, l2) {
     if (!l1 || !l2) return (l1 ? l1 : l2); // if (l1 === null) return l2; if (l2 === null) return l1;
 
     if (l1.val < l2.val) {
@@ -80,71 +89,31 @@ var mergeTwoLists2 = function (l1, l2) {
 console.log('mergeTwoSortedLists2', mergeTwoLists2(head1, head2));
 
 
-var mergeTwoLists3 = function (l1, l2) {
-    // simple error check
-    if (l1 == null && l2 == null) {
-        return null;
-    }
-
-    // a flag for if mergeHead has a value
-    // mergeHead and mergEnd (which we will actively add to)
-    var firstTime = true;
-    var mergeHead = new ListNode();
-    mergeEnd = mergeHead;
-
-    // if both
-    // pick the smaller one, and advance its pointer
-    while (l1 && l2) {
-        if (firstTime) { // first time only
-            firstTime = false;
-            if (l1.val < l2.val) {
-                mergeHead.val = l1.val;
-                l1 = l1.next;
-            } else {
-                mergeHead.val = l2.val;
-                l2 = l2.next;
-            }
-        } else {
-            if (l1.val < l2.val) {
-                mergeEnd.next = new ListNode(l1.val);
-                mergeEnd = mergeEnd.next;
-                l1 = l1.next;
-            } else {
-                mergeEnd.next = new ListNode(l2.val);
-                mergeEnd = mergeEnd.next;
-                l2 = l2.next;
-            }
-        }
-    }
-
-    // if only one, use it all the time
-    // keep in mind that we may end up directly here
-    // if the other starting head node is null, so we have to check firsTime again
-    while (l1) {
-        if (firstTime) {
-            firstTime = false;
-            mergeHead.val = l1.val;
-            l1 = l1.next;
-            continue;
-        }
-
-        mergeEnd.next = new ListNode(l1.val);
-        mergeEnd = mergeEnd.next;
-        l1 = l1.next;
-    }
-
-    while (l2) {
-        if (firstTime) {
-            firstTime = false;
-            mergeHead.val = l2.val;
-            l2 = l2.next;
-            continue;
-        }
-
-        mergeEnd.next = new ListNode(l2.val);
-        mergeEnd = mergeEnd.next;
-        l2 = l2.next;
-    }
-
-    return mergeHead;
-};
+// // Time: O( m * n ) Space: O(1)
+// const mergeTwoLists1 = (l1, l2) => {
+//     let node = l1;
+//     if (!l1) return l2;
+//     if (!l2) return l1;
+//     if (!l1 || !l2) return null;
+//     // move node to the last node of l1
+//     while (node.next) {
+//         node = node.next;
+//     }
+//     // connect l1 with l2
+//     node.next = l2;
+//     node = l1;
+//
+//     while (node) {
+//         let curr = node.next;
+//
+//         while (curr) {
+//             if (node.val > curr.val) {
+//                 [curr.val, node.val] = [node.val, curr.val];
+//             }
+//             curr = curr.next;
+//         }
+//         node = node.next;
+//     }
+//     return l1;
+// };
+//
