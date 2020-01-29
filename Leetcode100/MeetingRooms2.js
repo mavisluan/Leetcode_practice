5cda0662bbe1d1efa3da95f75cd108abf20dd022
Yi Angelov <mavisluan83@gmail.com>
2020-01-29 09:00:42 -0800

    Added Sorting solution to Meeting Rooms 1

--- Leetcode100/MeetingRooms2.js
+++ Leetcode100/MeetingRooms2.js
@@ -29,19 +29,42 @@ Output: true
  */
 // Solution 1: Bruce force
 //  Time: O(n^2) Space: O(1)
+// Since it's not sorted, 
+// consider two cases: 1)  a[0] >= b[0]   2) a[0] >= b[0]
+var canAttendMeetings = function(intervals) {
+    const size = intervals.length;
+
+    if (size <= 1) return true;
+    
+    for (let i = 0; i < size; i ++) {
+        for ( let j = i + 1; j < size; j ++) {
+            if (overLap(intervals[i], intervals[j])) return false;
+        
+        }
+    }
+
+    return true;
+};
+
+const overLap = (interval1, interval2) => {
+    return ((interval1[0] <= interval2[0] && interval2[0] < interval1[1])
+        || (interval1[0] >= interval2[0] && interval2[1] > interval1[0])
+    )
+}
+
+
+// Solution2 Sorting
+// Time: O(n Logn) Space: O(1)
 // 1. Sort the intervals array by meeting's starting time (intervals[i][0])
 // 2. If the first meeting's end time > the next meeting's start time --> return false
- 
 var canAttendMeetings = function(intervals) {
     const size = intervals.length;
 
     if (size <= 1) return true;
 
     intervals.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i < size; i ++) {
-        for ( let j = i + 1; j < size; j ++) {
-            if (intervals[i][1] > intervals[j][0]) return false;
-        }
+    for (let i = 0; i < size - 1; i ++) {
+       if (intervals[i][1] > intervals[i+1][0]) return false;
     }
 
     return true;
