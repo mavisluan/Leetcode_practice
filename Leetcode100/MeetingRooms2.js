8fb3131e0688c0d51fa1e5a9e056fd4fcb028d93
Yi Angelov <mavisluan83@gmail.com>
2020-01-30 14:50:36 -0800

    Added unit-tests for meetingRooms2

--- Leetcode100/MeetingRooms2.js
+++ Leetcode100/MeetingRooms2.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /*
 253. Meeting Rooms II    Medium
 Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
@@ -12,7 +13,6 @@ Input: [[7,10],[2,4]]
 Output: 1
 
  */
-
 
 /*
 252. Meeting Rooms
@@ -29,43 +29,43 @@ Output: true
  */
 // Solution 1: Bruce force
 //  Time: O(n^2) Space: O(1)
-// Since it's not sorted, 
+// Since it's not sorted,
 // consider two cases: 1)  a[0] >= b[0]   2) a[0] >= b[0]
-var canAttendMeetings = function(intervals) {
-    if (intervals === null || intervals.length <=1) return true;
+
+const overLap = (interval1, interval2) =>
+    // If the earliest end time is bigger than the latest start time, overLap is true
+    Math.max(interval1[0], interval2[0]) < Math.min(interval1[1], interval2[1]);
+
+const canAttendMeetings = function(intervals) {
+    if (intervals === null || intervals.length <= 1) return true;
     const size = intervals.length;
-    
-    for (let i = 0; i < size; i ++) {
-        for ( let j = i + 1; j < size; j ++) {
+
+    for (let i = 0; i < size; i++) {
+        for (let j = i + 1; j < size; j++) {
             if (overLap(intervals[i], intervals[j])) return false;
-        
         }
     }
 
     return true;
 };
 
-const overLap = (interval1, interval2) => {
-    // return ((interval1[0] <= interval2[0] && interval2[0] < interval1[1])
-    //     || (interval1[0] >= interval2[0] && interval2[1] > interval1[0])
-    // )
-    // If the earliest end time is bigger than the latest start time, overLap is true
-    return Math.max(interval1[0], interval2[0]) < Math.min(interval1[1], interval2[1]);
-    }
-
-
 // Solution2 Sorting
 // Time: O(n Logn) Space: O(1)
 // 1. Sort the intervals array by meeting's starting time (intervals[i][0])
 // 2. If the first meeting's end time > the next meeting's start time --> return false
-var canAttendMeetings = function(intervals) {
-    if (intervals === null || intervals.length <=1) return true;
+const canAttendMeetings2 = function(intervals) {
+    if (intervals === null || intervals.length <= 1) return true;
     const size = intervals.length;
 
     intervals.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i < size - 1; i ++) {
-       if (intervals[i][1] > intervals[i+1][0]) return false;
+    for (let i = 0; i < size - 1; i++) {
+        if (intervals[i][1] > intervals[i + 1][0]) return false;
     }
 
     return true;
+};
+
+module.exports = {
+    canAttendMeetings,
+    canAttendMeetings2,
 };
---
+++ tests/meetingRooms2.test.js
@@ -0,0 +1,47 @@
+const { assert } = require('chai');
+const { canAttendMeetings, canAttendMeetings2 } = require('../Leetcode100/meetingRooms2');
+
+const test = fn => {
+    describe('MeetingRooms', () => {
+        describe('MeetingRooms1', () => {
+            it('Should return a boolean', () => {
+                assert.typeOf(
+                    fn([
+                        [0, 30],
+                        [5, 10],
+                        [15, 20],
+                    ]),
+                    'boolean'
+                );
+            });
+
+            it('Should return false when meetings overlap', () => {
+                assert.equal(
+                    fn([
+                        [0, 30],
+                        [5, 10],
+                        [15, 20],
+                    ]),
+                    false
+                );
+            });
+
+            it('Should return true when meetings do not overlap', () => {
+                assert.equal(
+                    fn([
+                        [7, 10],
+                        [2, 4],
+                    ]),
+                    true
+                );
+            });
+
+            it('Should return true when intervals are empty', () => {
+                assert.equal(fn([]), true);
+            });
+        });
+    });
+};
+
+test(canAttendMeetings);
+test(canAttendMeetings2);
