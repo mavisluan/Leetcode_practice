7a4f9c91805679eca9aaaa4a3d2265a56a06df21
Yi Angelov <mavisluan83@gmail.com>
2020-01-31 13:03:23 -0800

    Added meetingRooms2 and test cases

--- Leetcode100/MeetingRooms2.js
+++ Leetcode100/MeetingRooms2.js
@@ -18,69 +18,32 @@ const minMeetingRooms = function(intervals) {
     const size = intervals.length;
     if (size <= 1) return size;
 
-    intervals.sort((a, b) => a[0] - b[0]);
     let counter = 1;
-    for (let i = 0; i < size - 1; i++) {
-        if (intervals[i][1] > intervals[i + 1][0]) counter++;
+    const [starts, ends] = [[], []];
+
+    intervals.forEach(int => {
+        starts.push(int[0]);
+        ends.push(int[1]);
+    });
+
+    starts.sort((a, b) => a - b);
+    ends.sort((a, b) => a - b);
+
+    let [s, e] = [1, 0];
+
+    while (s < size && e < size) {
+        if (starts[s] < ends[e]) {
+            counter++;
+        } else {
+            e++;
+        }
+
+        s++;
     }
 
     return counter;
 };
 
-/*
-252. Meeting Rooms
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
-
-Example 1:
-
-Input: [[0,30],[5,10],[15,20]]
-Output: false
-Example 2:
-
-Input: [[7,10],[2,4]]
-Output: true
- */
-// Solution 1: Bruce force
-//  Time: O(n^2) Space: O(1)
-// Since it's not sorted,
-// consider two cases: 1)  a[0] >= b[0]   2) a[0] >= b[0]
-
-const overLap = (interval1, interval2) =>
-    // If the earliest end time is bigger than the latest start time, overLap is true
-    Math.max(interval1[0], interval2[0]) < Math.min(interval1[1], interval2[1]);
-
-const canAttendMeetings = function(intervals) {
-    if (intervals === null || intervals.length <= 1) return true;
-    const size = intervals.length;
-
-    for (let i = 0; i < size; i++) {
-        for (let j = i + 1; j < size; j++) {
-            if (overLap(intervals[i], intervals[j])) return false;
-        }
-    }
-
-    return true;
-};
-
-// Solution2 Sorting
-// Time: O(n Logn) Space: O(1)
-// JS sort function: For arrays containing 10 or fewer elements, time complexity of .sort is O(n^2), and space complexity is O(1). For longer arrays time complexity is Θ(n log(n)) (average case), and space complexity is O(log(n))
-// 1. Sort the intervals array by meeting's starting time (intervals[i][0])
-// 2. If the first meeting's end time > the next meeting's start time --> return false
-const canAttendMeetings2 = function(intervals) {
-    if (intervals === null || intervals.length <= 1) return true;
-    const size = intervals.length;
-
-    intervals.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i < size - 1; i++) {
-        if (intervals[i][1] > intervals[i + 1][0]) return false;
-    }
-
-    return true;
-};
-
 module.exports = {
-    canAttendMeetings,
-    canAttendMeetings2,
     minMeetingRooms,
 };
--- tests/meetingRooms2.test.js
+++ tests/meetingRooms2.test.js
@@ -1,89 +1,49 @@
 const { assert } = require('chai');
-const { canAttendMeetings, canAttendMeetings2, minMeetingRooms } = require('../Leetcode100/meetingRooms2');
+const { minMeetingRooms } = require('../Leetcode100/meetingRooms2');
 
-const test = fn1 => {
-    describe('MeetingRooms', () => {
-        describe('MeetingRooms1', () => {
-            it('Should return a boolean', () => {
-                assert.typeOf(
-                    fn1([
-                        [0, 30],
-                        [5, 10],
-                        [15, 20],
-                    ]),
-                    'boolean'
-                );
-            });
+const test = fn => {
+    describe('MeetingRooms2', () => {
+        it('Should return a number', () => {
+            assert.typeOf(
+                fn([
+                    [0, 30],
+                    [5, 10],
+                    [15, 20],
+                ]),
+                'number'
+            );
+        });
 
-            it('Should return false when meetings overlap', () => {
-                assert.equal(
-                    fn1([
-                        [0, 30],
-                        [5, 10],
-                        [15, 20],
-                    ]),
-                    false
-                );
-            });
+        it('Should return 2 when 2 meetings overlap', () => {
+            assert.equal(
+                fn([
+                    [0, 30],
+                    [5, 10],
+                    [15, 20],
+                ]),
+                2
+            );
+        });
 
-            it('Should return true when meetings do not overlap', () => {
-                assert.equal(
-                    fn1([
-                        [7, 10],
-                        [2, 4],
-                    ]),
-                    true
-                );
-            });
+        it('Should return one when meetings do not overlap', () => {
+            assert.equal(
+                fn([
+                    [7, 10],
+                    [2, 4],
+                ]),
+                1
+            );
+        });
 
-            it('Should return true when intervals are empty', () => {
-                assert.equal(fn1([]), true);
-            });
+        it('Should return 0 when intervals are empty', () => {
+            assert.equal(fn([]), 0);
+        });
+
+        it('Should return 0 when intervals are empty', () => {
+            assert.equal(fn(null), 0);
         });
     });
 };
 
-test(canAttendMeetings);
-test(canAttendMeetings2);
-
-describe('MeetingRooms2', () => {
-    it('Should return a number', () => {
-        assert.typeOf(
-            minMeetingRooms([
-                [0, 30],
-                [5, 10],
-                [15, 20],
-            ]),
-            'number'
-        );
-    });
-
-    it('Should return 2 when 2 meetings overlap', () => {
-        assert.equal(
-            minMeetingRooms([
-                [0, 30],
-                [5, 10],
-                [15, 20],
-            ]),
-            2
-        );
-    });
-
-    it('Should return one when meetings do not overlap', () => {
-        assert.equal(
-            minMeetingRooms([
-                [7, 10],
-                [2, 4],
-            ]),
-            1
-        );
-    });
-
-    it('Should return 0 when intervals are empty', () => {
-        assert.equal(minMeetingRooms([]), 0);
-    });
-
-    it('Should return 0 when intervals are empty', () => {
-        assert.equal(minMeetingRooms(null), 0);
-    });
-});
+test(minMeetingRooms);
+test(minMeetingRooms);
