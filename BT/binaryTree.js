b0080625350866ef916316e31c0bbabe8343cd3b
yi <mavisluan83@gmail.com>
2020-02-08 10:27:31 -0800

    Added serializeDeserialzieDFS

--- BT/binaryTree.js
+++ BT/binaryTree.js
@@ -1,6 +1,18 @@
+/* eslint-disable no-console */
 // Definition for a binary tree node.
 function TreeNode(val) {
     this.val = val;
     this.left = null;
     this.right = null;
 }
+
+const testRoot = new TreeNode(1);
+testRoot.left = new TreeNode(2);
+testRoot.right = new TreeNode(5);
+const node2 = testRoot.left;
+node2.left = new TreeNode(3);
+node2.right = new TreeNode(4);
+
+// console.log('testRoot', testRoot);
+
+module.exports = { TreeNode, testRoot };
--- Leetcode100/serializeDeserializeDFS.js
+++ Leetcode100/serializeDeserializeDFS.js
@@ -1,4 +1,8 @@
-const serializeDeserializeDFS = root => {
+/* eslint-disable no-shadow */
+/* eslint-disable no-console */
+const { testRoot, TreeNode } = require('../BT/binaryTree');
+
+const serializeDFS = root => {
     if (!root) return [];
 
     const result = [];
@@ -14,5 +18,27 @@ const serializeDeserializeDFS = root => {
     };
 
     helper(root);
+    console.log('result', result);
     return result;
 };
+
+const deserializeDFS = data => {
+    if (data.length === 0) return null;
+
+    const root = new TreeNode(data.shift());
+    const helper = node => {
+        if (node === null) return null;
+        const leftVal = data.shift();
+        node.left = helper(leftVal === null ? null : new TreeNode(leftVal));
+        const rightVal = data.shift();
+        node.right = helper(rightVal === null ? null : new TreeNode(rightVal));
+        return node;
+    };
+
+    helper(root);
+
+    return root;
+};
+
+const result = serializeDFS(testRoot);
+deserializeDFS(result);
