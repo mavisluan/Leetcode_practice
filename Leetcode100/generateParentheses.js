dadd6020669933ad54f4a80a7992a999a130ea5c
Yi Angelov <mavisluan83@gmail.com>
2020-02-10 10:53:20 -0800

    Added generateParentheses

---
+++ Leetcode100/generateParentheses.js
@@ -0,0 +1,49 @@
+/**
+22. Generate Parentheses         Medium
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+ */
+
+/** Time complexity analysis From Jack-O
+  * The way I like to think about the runtime of backtracking algorithms is O(b^d),
+  *  where b is the branching factor and d is the maximum depth of recursion.
+Backtracking is characterized by a number of decisions b that can be made at each level of recursion. 
+If you visualize the recursion tree, this is the number of children each internal node has. 
+You can also think of b as standing for "base", which can help you remember that b is the base of the exponential.
+
+If we can make b decisions at each level of recursion, and we expand the recursion tree to d levels (ie: each path has a length of d), 
+then we get b^d nodes. Since backtracking is exhaustive and must visit each one of these nodes, the runtime is O(b^d).
+ */
+
+function generateParenthesis(n) {
+    const res = [];
+
+    function go(l, r, s) {
+        console.log(`current str is ${s}, l:${l}, r:${r}`);
+        // l: left remaining, r: right remaining
+        if (l > r) return; // The number of '(' should be always >= ')'
+
+        if (l === 0 && r === 0) {
+            res.push(s);
+            return;
+        }
+
+        if (l > 0) go(l - 1, r, `${s}(`);
+        if (r > 0) go(l, r - 1, `${s})`);
+    }
+
+    go(n, n, '');
+    return res;
+}
+
+console.log('generateParenthesis', generateParenthesis(3));
