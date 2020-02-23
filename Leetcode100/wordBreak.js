d1287f18896d241513f302072f41e1c46b05b711
yi <mavisluan83@gmail.com>
2020-02-22 21:28:57 -0800

    Added dp solution

--- Leetcode100/wordBreak.js
+++ Leetcode100/wordBreak.js
@@ -16,3 +16,21 @@ Example 3:
 Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 Output: false
 */
+
+// Solution 1 DP
+const wordBreak = (s, wordDict) => {
+    const dp = new Array(s.length + 1).fill(false);
+    dp[0] = true;
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = 0; j < i; j++) {
+            const word = s.slice(j, i);
+            if (dp[j] === true && wordDict.includes(word)) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[s.length];
+};
