d8d198896896ab291430d7206ca3f1e607e16f0a
yi <mavisluan83@gmail.com>
2020-02-29 15:41:13 -0800

    Added comments for categorize solution

--- Leetcode100/groupAnagrams.js
+++ Leetcode100/groupAnagrams.js
@@ -17,12 +17,20 @@ All inputs will be in lowercase.
 The order of your output does not matter.
  */
 
+/* Solution 1 Categorize by Sorted string
+    Maintain a map group: {string --> array} where
+    K is a sorted string, and each value is the array of strings 
+    from the initial input 
+*/
+// Time: O(NK logK) --> N - length of strs, K - max length of a string in strs
+// Space: O(NK) --> The total info content stored in group
 const groupAnagrams = function(strs) {
     const groups = {};
     for (const s of strs) {
+        // Time: O(N)
         const key = s
             .split('')
-            .sort()
+            .sort() // Time: O(K logK)
             .join('');
         if (groups[key]) {
             groups[key] = [...groups[key], s];
