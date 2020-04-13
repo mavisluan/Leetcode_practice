ad3132151766f3a5290e270bafc573b5e0a253da
yi <mavisluan83@gmail.com>
2020-04-12 18:06:38 -0700

    844 backspaceStringCompare

---
+++ problems/844_backspaceStringCompare.js
@@ -0,0 +1,56 @@
+/**
+ *844. Backspace String Compare
+Easy
+
+1415
+
+73
+
+Add to List
+
+Share
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+Note:
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S and T only contain lowercase letters and '#' characters.
+Follow up:
+
+Can you solve it in O(N) time and O(1) space?
+ */
+
+// Solution1 Stack
+// Time: O(M+N)  Space: O(M+N) - build a new stack
+const backspaceCompare = (S, T) => {
+  const build = (str) => {
+    const stack = [];
+    for (const char of str.split('')) {
+      (char === '#') ? stack.pop() : stack.push(char);
+    }
+    return stack.join('');
+  };
+
+  return build(S) === build(T);
+};
