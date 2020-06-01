913497561241f126cd12eda827722dfc74e432ff
yi <mavisluan83@gmail.com>
2020-05-31 22:25:17 -0700

    206. Reverse Linked List -- Easy

--- problems/206_revertLinkedList.js
+++ problems/206_revertLinkedList.js
@@ -1,3 +1,13 @@
+/*
+206. Reverse Linked List --Easy
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+ */
+
 function ListNode(val) {
     this.val = val;
     this.next = null;
